414ab7474647098436cb879f1ea6f193d2f48c47
Gary Oberbrunner <garyo@genarts.com>
2012-12-21 09:58:58 -0500

    Use zsh as explicit shell, but allow it to be found in path.

--- init.el
+++ init.el
@@ -86,7 +86,7 @@
 
 ;; use zsh or bash.  Do this early on before loading any git stuff,
 ;; otherwise that will try to use cmdproxy.exe.
-(setq explicit-shell-file-name "c:/bin/zsh")
+(setq explicit-shell-file-name "zsh")
 (setq shell-file-name explicit-shell-file-name)
 
 (load-library "paren")
