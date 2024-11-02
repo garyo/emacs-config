1410ff255b071785c00ac21b2faf7fe5f15fa342
Gary Oberbrunner <garyo@darkstarsystems.com>
2024-11-02 10:50:35 -0400

    casual: new package structure

--- lisp/init-casual.el
+++ lisp/init-casual.el
@@ -4,58 +4,47 @@
 
 ;;; All the casual packages
 
-(use-package casual-calc
-  :bind (:map calc-mode-map ("C-o" . casual-calc-tmenu)))
-
-(use-package casual-info
-  :bind (:map Info-mode-map ("C-o" . casual-info-tmenu)))
-
-(use-package casual-dired
-  :bind (:map dired-mode-map ("C-o" . casual-dired-tmenu)))
-
-(use-package casual-avy
-  :bind ("M-G" . casual-avy-tmenu))
-
-(use-package casual-isearch
-  :bind (:map isearch-mode-map ("<f2>" . casual-isearch-tmenu)))
-
+;;; Packages that casual hooks into:
 (use-package ibuffer
   :hook (ibuffer-mode . ibuffer-auto-mode)
   :ensure nil                           ; built-in
   :defer t)
-(use-package casual-ibuffer
-  :bind (:map
-         ibuffer-mode-map
-         ("C-o" . casual-ibuffer-tmenu)
-         ("F" . casual-ibuffer-filter-tmenu)
-         ("s" . casual-ibuffer-sortby-tmenu)
-         ("<double-mouse-1>" . ibuffer-visit-buffer) ; optional
-         ("M-<double-mouse-1>" . ibuffer-visit-buffer-other-window) ; optional
-         ("{" . ibuffer-backwards-next-marked) ; optional
-         ("}" . ibuffer-forward-next-marked)   ; optional
-         ("[" . ibuffer-backward-filter-group) ; optional
-         ("]" . ibuffer-forward-filter-group)  ; optional
-         ("$" . ibuffer-toggle-filter-group))  ; optional
-  :after (ibuffer))
 
 (use-package re-builder
   :ensure nil
   :defer t)
-(use-package casual-re-builder
-  :bind (:map
-         reb-mode-map ("C-o" . casual-re-builder-tmenu)
-         :map
-         reb-lisp-mode-map ("C-o" . casual-re-builder-tmenu))
-  :after (re-builder))
-
 (use-package bookmark
   :ensure nil
   :defer t)
-(use-package casual-bookmarks
-  :bind (:map bookmark-bmenu-mode-map
-              ("C-o" . casual-bookmarks-tmenu)
-              ("S" . casual-bookmarks-sortby-tmenu)
-              ("J" . bookmark-jump))
-  :after (bookmark))
+
+(use-package casual
+  :after (ibuffer re-builder bookmark)
+  :bind
+  (:map calc-mode-map ("C-o" . casual-calc-tmenu))
+  (:map Info-mode-map ("C-o" . casual-info-tmenu))
+  (:map dired-mode-map ("C-o" . casual-dired-tmenu))
+  ("M-G" . casual-avy-tmenu)
+  (:map isearch-mode-map ("<f2>" . casual-isearch-tmenu))
+  (:map
+   ibuffer-mode-map
+   ("C-o" . casual-ibuffer-tmenu)
+   ("F" . casual-ibuffer-filter-tmenu)
+   ("s" . casual-ibuffer-sortby-tmenu)
+   ("<double-mouse-1>" . ibuffer-visit-buffer) ; optional
+   ("M-<double-mouse-1>" . ibuffer-visit-buffer-other-window) ; optional
+   ("{" . ibuffer-backwards-next-marked)       ; optional
+   ("}" . ibuffer-forward-next-marked)         ; optional
+   ("[" . ibuffer-backward-filter-group)       ; optional
+   ("]" . ibuffer-forward-filter-group)        ; optional
+   ("$" . ibuffer-toggle-filter-group))
+  (:map
+   reb-mode-map ("C-o" . casual-re-builder-tmenu)
+   :map
+   reb-lisp-mode-map ("C-o" . casual-re-builder-tmenu))
+  (:map bookmark-bmenu-mode-map
+        ("C-o" . casual-bookmarks-tmenu)
+        ("S" . casual-bookmarks-sortby-tmenu)
+        ("J" . bookmark-jump))
+  )
 
 (provide 'init-casual)
