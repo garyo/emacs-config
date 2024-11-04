d098496bcc9be7dfccad7e1f32d3146d09fc46dd
Gary Oberbrunner <garyo@darkstarsystems.com>
2024-11-04 14:25:08 -0500

    Allow all themes without prompting, and update elpaca bootstrap version to 0.8
    Signed-off-by: Gary Oberbrunner <garyo@darkstarsystems.com>

--- lisp/elpaca-bootstrap.el
+++ lisp/elpaca-bootstrap.el
@@ -6,7 +6,7 @@
 ;;; Bootstrap: load elpaca.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "var/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
